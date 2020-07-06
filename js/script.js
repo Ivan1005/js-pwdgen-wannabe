15b9ce816b41a62d40b88e1a8bb42fdc070115eb
Ivan <matteraivam@gmail.com>
2020-07-06 14:54:37 +0200

    esercizio

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var name = prompt("nome") ;
+var cognome = prompt("cognome");
+var colorePreferito = prompt("colore preferito");
+
+document.getElementById("info").innerHTML ="La tua password: " + name + cognome + colorePreferito + 19;
